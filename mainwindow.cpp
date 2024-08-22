deb219352f73d22c330ca6b56d9b9dae0a566d6e
neffatimohamedsaid7 <neffati.mohamedsaid@esprit.tn>
2024-08-22 17:44:43 +0100

    controle saisie

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -48,6 +48,14 @@ MainWindow::~MainWindow()
 
     void MainWindow::on_pushButton_Ajouter_clicked()
     {
+        // Réinitialisez les étiquettes d'erreur à vide
+            ui->label_ID_candidat->setText("");
+            ui->label_nom->setText("");
+            ui->label_prenom->setText("");
+            ui->label_datenaissance->setText("");
+            ui->label_niveau->setText("");
+            ui->label_email->setText("");
+            ui->label_numtel->setText("");
 
         int ID_candidat=ui->lineEdit_ID_candidat->text().toInt();
         QString nom=ui->lineEdit_nom->text();
@@ -56,7 +64,53 @@ MainWindow::~MainWindow()
         QString niveau=ui->comboBox_niveaucandidat->currentText();
         QString email=ui->lineEdit_email->text();
         QString numtel=ui->lineEdit_numtel->text();
-        CANDIDATS C(ID_candidat,nom, prenom, datenaissance, niveau,email,numtel);
+        bool isValid = true;
+        QString ID_candidatString = QString::number(ID_candidat);
+        if (ID_candidatString.isEmpty()) {
+                ui->label_ID_candidat->setText("<font color='red'>ID_candidat ne peut pas être vide!!!!</font>");
+                isValid = false;
+            }
+
+            if (nom.isEmpty()) {
+                ui->label_nom->setText("<font color='red'>nom ne peut pas être vide!!!</font>");
+                isValid = false;
+            }
+
+            if (prenom.isEmpty()) {
+                ui->label_prenom->setText("<font color='red'>prenom ne peut pas être vide!!!</font>");
+                isValid = false;
+            }
+            QString datenaissanceString = datenaissance.toString("yyyy-MM-dd");
+            if (datenaissanceString.isEmpty()) {
+                ui->label_datenaissance->setText("<font color='red'>datenaissance ne peut pas être vide!!!</font>");
+                isValid = false;
+            }
+            if (niveau.isEmpty()) {
+                ui->label_niveau->setText("<font color='red'>niveau ne peut pas être vide!!!</font>");
+                isValid = false;
+            }
+            if (email.isEmpty()) {
+                ui->label_email->setText("<font color='red'>email ne peut pas être vide!!!</font>");
+                isValid = false;
+            }
+            if (numtel.isEmpty()) {
+                ui->label_numtel->setText("<font color='red'>numtel ne peut pas être vide!!!</font>");
+                isValid = false;
+            }
+
+            QSqlQuery checkQuery;
+            checkQuery.prepare("SELECT ID_candidat FROM CANDIDATS WHERE ID_candidat = :ID_candidat");
+            checkQuery.bindValue(":ID_candidat", ID_candidat);
+            if (checkQuery.exec() && checkQuery.next()) {
+                ui->label_ID_candidat->setText("<font color='red'>ID_candidat existe déjà!!!!</font>");
+                isValid = false;
+            }
+
+
+            if (isValid) {
+                // Si l'entrée est valide et que l'ID_candidat n'existe pas, procédez à l'ajout de CANDIDATS
+                 CANDIDATS C(ID_candidat,nom, prenom, datenaissance, niveau,email,numtel);
+
         bool test=C.ajouter();
         if(test)
        {
@@ -70,7 +124,15 @@ MainWindow::~MainWindow()
          ui->lineEdit_nom->clear();
          ui->lineEdit_prenom->clear();
          ui->lineEdit_email->clear();
-         ui->lineEdit_numtel->clear(); }
+         ui->lineEdit_numtel->clear();
+
+         ui->label_ID_candidat->clear();
+         ui->label_nom->clear();
+         ui->label_prenom->clear();
+         ui->label_email->clear();
+         ui->label_numtel->clear();
+         ui->label_ID_candidat_2->clear();
+        }
 
 
         else
@@ -78,7 +140,7 @@ MainWindow::~MainWindow()
          QObject::tr("ajout non effectué.\n"
                      "Click Cancel to exit."), QMessageBox::Cancel);
     }
-
+}
 
 
 void MainWindow::on_pushButton_Supprimer_clicked()
@@ -110,40 +172,121 @@ void MainWindow::on_pushButton_Supprimer_clicked()
          QObject::tr("suppression non effectué.\n"
                      "Click Cancel to exit."), QMessageBox::Cancel);
 
+
     }
 
 void MainWindow::on_pushButton_Ajouter2_clicked()
 {
+    // Réinitialisez les étiquettes d'erreur à vide
+    ui->label_ID_OffreEmploi->setText("");
+    ui->label_titre->setText("");
+    ui->label_description->setText("");
+    ui->label_datepublication->setText("");
+    ui->label_nbrplace->setText("");
+    ui->label_lieu->setText("");
+    ui->label_ID_candidat_2->setText("");
 
-    int ID_OffreEmploi=ui->lineEdit_ID_OffreEmploi->text().toInt();
-    QString titre=ui->lineEdit_titre->text();
-    QString description=ui->lineEdit_description->text();
-    QDate datepublication = ui->lineEdit_datepublication->date();
-    int nbrplace=ui->lineEdit_ID_nbrplace->text().toInt();
-    QString lieu=ui->comboBox_lieuOffreEmploi->currentText();
-    int ID_candidat=ui->lineEdit_ID_candidat_2->text().toInt();
-    OFFRES_EMPLOIS O(ID_OffreEmploi,titre, description, datepublication, nbrplace,lieu,ID_candidat);
-    bool test=O.ajouter();
-    if(test)
-   {
-        ui->tableView_OffresEmplois->setModel(O.afficher());
+    // Récupérer les valeurs des champs
+    QString ID_OffreEmploiStr = ui->lineEdit_ID_OffreEmploi->text();
+    QString titre = ui->lineEdit_titre->text();
+    QString description = ui->lineEdit_description->text();
+    QDate datepublication = ui->lineEdit_datepublication->date();  // Utilisez dateEdit au lieu de lineEdit
+    QString nbrplaceStr = ui->lineEdit_nbrplace->text();
+    QString lieu = ui->comboBox_lieuOffreEmploi->currentText();
+    QString ID_candidat_2Str = ui->lineEdit_ID_candidat_2->text();
 
-      QMessageBox::information(nullptr, QObject::tr("ok"),
-      QObject::tr("ajout effectué.\n"
-                   "Click Cancel to exit."), QMessageBox::Cancel);
+    bool isValid = true;
 
-     ui->lineEdit_ID_OffreEmploi->clear();
-     ui->lineEdit_titre->clear();
-     ui->lineEdit_datepublication->clear();
-     ui->lineEdit_ID_nbrplace->clear();
-     ui->lineEdit_ID_candidat_2->clear(); }
+    // Validation des champs
+    if (ID_OffreEmploiStr.isEmpty()) {
+        ui->label_ID_OffreEmploi->setText("<font color='red'>ID_OffreEmploi ne peut pas être vide!!!!</font>");
+        isValid = false;
+    }
+
+    if (titre.isEmpty()) {
+        ui->label_titre->setText("<font color='red'>titre ne peut pas être vide!!!</font>");
+        isValid = false;
+    }
+
+    if (description.isEmpty()) {
+        ui->label_description->setText("<font color='red'>description ne peut pas être vide!!!</font>");
+        isValid = false;
+    }
+
+    QString datepublicationStr = datepublication.toString("yyyy-MM-dd");
+    if (datepublicationStr.isEmpty()) {
+        ui->label_datepublication->setText("<font color='red'>Date de publication ne peut pas être vide!!!</font>");
+        isValid = false;
+    }
+
+    if (nbrplaceStr.isEmpty()) {
+        ui->label_nbrplace->setText("<font color='red'>Nombre de places ne peut pas être vide!!!</font>");
+        isValid = false;
+    }
+
+    if (lieu.isEmpty()) {
+        ui->label_lieu->setText("<font color='red'>Lieu ne peut pas être vide!!!</font>");
+        isValid = false;
+    }
+
+    if (ID_candidat_2Str.isEmpty()) {
+        ui->label_ID_candidat_2->setText("<font color='red'>ID Candidat ne peut pas être vide!!!</font>");
+        isValid = false;
+    }
+    int ID_OffreEmploi = ID_OffreEmploiStr.toInt();
+    int nbrplace = nbrplaceStr.toInt();
+    int ID_candidat_2 = ID_candidat_2Str.toInt();
+    QSqlQuery checkQuery;
+    checkQuery.prepare("SELECT ID_OffreEmploi FROM OFFRES_EMPLOIS WHERE ID_OffreEmploi = :ID_OffreEmploi");
+    checkQuery.bindValue(":ID_OffreEmploi", ID_OffreEmploi);
+
+    if (!checkQuery.exec()) {
+        qDebug() << "Erreur lors de l'exécution de la requête: " << checkQuery.lastError().text();
+    } else if (checkQuery.next()) {
+        qDebug() << "ID exists: " << checkQuery.value(0).toString();  // Debug output
+        ui->label_ID_OffreEmploi->setText("<font color='red'>ID Offre Emploi existe déjà!!!!</font>");
+        return; // Retourne ici si l'ID existe déjà pour éviter d'exécuter le reste du code
+    }
+
+    // Si tous les champs sont valides, vérifiez l'existence de l'ID dans la base de données
+    if (isValid) {
 
 
-    else
-     QMessageBox::critical(nullptr, QObject::tr("not ok"),
-     QObject::tr("ajout non effectué.\n"
-                 "Click Cancel to exit."), QMessageBox::Cancel);
+
+        // Si l'ID n'existe pas, ajouter l'offre
+        OFFRES_EMPLOIS O(ID_OffreEmploi, titre, description, datepublication, nbrplace, lieu, ID_candidat_2);
+        bool test = O.ajouter();
+        if (test) {
+            ui->tableView_OffresEmplois->setModel(O.afficher());
+
+            QMessageBox::information(nullptr, QObject::tr("ok"),
+                                     QObject::tr("Ajout effectué.\n"
+                                                 "Click Cancel to exit."), QMessageBox::Cancel);
+
+            // Réinitialiser les champs après un ajout réussi
+            ui->lineEdit_ID_OffreEmploi->clear();
+            ui->lineEdit_titre->clear();
+            ui->lineEdit_description->clear();
+            ui->lineEdit_nbrplace->clear();
+            ui->comboBox_lieuOffreEmploi->setCurrentIndex(0);
+            ui->lineEdit_ID_candidat_2->clear();
+
+            // Effacer les labels d'erreur
+            ui->label_ID_OffreEmploi->clear();
+            ui->label_titre->clear();
+            ui->label_description->clear();
+            ui->label_datepublication->clear();
+            ui->label_nbrplace->clear();
+            ui->label_lieu->clear();
+            ui->label_ID_candidat_2->clear();
+        } else {
+            QMessageBox::critical(nullptr, QObject::tr("not ok"),
+                                  QObject::tr("Ajout non effectué.\n"
+                                              "Click Cancel to exit."), QMessageBox::Cancel);
+        }
+    }
 }
+
 
 
 
@@ -218,7 +361,7 @@ void MainWindow::on_tableView_OffresEmplois_clicked(const QModelIndex &index)
         ui->lineEdit_titre->setText(titre);
         ui->lineEdit_description->setText(description);
         ui->lineEdit_datepublication->setDate(datepublication);
-        ui->lineEdit_ID_nbrplace->setText(QString::number(nbrplace));
+        ui->lineEdit_nbrplace->setText(QString::number(nbrplace));
         ui->comboBox_lieuOffreEmploi->currentText();
         ui->lineEdit_ID_candidat_2->setText(QString::number(ID_candidat_2));
 }
@@ -229,7 +372,7 @@ void MainWindow::on_pushButton_Modifier_clicked()
     QString nom=ui->lineEdit_titre->text();
     QString prenom=ui->lineEdit_description->text();
     QDate datenaissance = ui->lineEdit_datepublication->date();
-    QString niveau=ui->lineEdit_ID_nbrplace->text();
+    QString niveau=ui->lineEdit_nbrplace->text();
     QString email=ui->comboBox_lieuOffreEmploi->currentText();
     QString numtel=ui->lineEdit_ID_candidat_2->text();
     CANDIDATS C(ID_candidat,nom, prenom, datenaissance, niveau,email,numtel);
@@ -264,7 +407,7 @@ void MainWindow::on_pushButton_Modifier2_clicked()
         QString titre=ui->lineEdit_titre->text();
         QString description=ui->lineEdit_description->text();
         QDate datepublication = ui->lineEdit_datepublication->date();
-        int nbrplace=ui->lineEdit_ID_nbrplace->text().toInt();
+        int nbrplace=ui->lineEdit_nbrplace->text().toInt();
         QString lieu=ui->comboBox_lieuOffreEmploi->currentText();
         int ID_candidat=ui->lineEdit_ID_candidat_2->text().toInt();
         OFFRES_EMPLOIS O(ID_OffreEmploi,titre, description, datepublication, nbrplace,lieu,ID_candidat);
@@ -281,7 +424,7 @@ void MainWindow::on_pushButton_Modifier2_clicked()
          ui->lineEdit_titre->clear();
          ui->lineEdit_description->clear();
          ui->lineEdit_datepublication->clear();
-         ui->lineEdit_ID_nbrplace->clear();
+         ui->lineEdit_nbrplace->clear();
          ui->lineEdit_ID_candidat_2->clear(); }
 
 
